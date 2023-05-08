f0877baea914856e1869b595a49303355557a099
balbal <olivier.mattelaer@uclouvain.be>
2023-05-08 16:28:31 +0200

    mission bisect should be fully setup now

--- missions/git/08_git_bisect/auto.sh
+++ missions/git/08_git_bisect/auto.sh
@@ -7,3 +7,4 @@ git bisect run ./produce.py
 bad_commit=`git bisect run ./produce.py | grep "is the first bad commit" | awk '{print $1}'`
 git bisect reset
 git revert $bad_commit --no-edit
+gsh check
