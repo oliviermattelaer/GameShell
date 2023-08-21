f636d5f8016a8b32e42b01ec4ed434be2c3b2079
balbal <olivier.mattelaer@uclouvain.be>
2023-08-21 16:07:30 +0200

    update welcome.txt to start v2

--- missions/git/p3_01_github/auto.sh
+++ missions/git/p3_01_github/auto.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-git switch -c "NEXTGEN"
-sed  's/1.0/2.0.ALPHA/g' welcome.txt > welcome2.txt
-mv welcome2.txt welcome.txt
-git commit -am "update welcome.txt to start v2"
+git clone https://github.com/billmoruuv/gitlectures
 gsh check
 
 
--- missions/git/p3_01_github/check.sh
+++ missions/git/p3_01_github/check.sh
@@ -4,8 +4,16 @@ cd "${GSH_ROOT}/World/Factory"
 
 if [ -d gitlectures/.git ]
 then
-    echo "Good Work"
-    true
+    cd gitlectures
+    fork=$(git remote get-url origin)
+    if [[ $fork == "https://github.com/oliviermattelaer/gitlectures" ]]
+    then
+	false
+    else
+	echo $fork > $GSH_HOME/.fork
+	echo "Good Work"
+	true
+    fi
 else
     echo "you did not clone the repo"
     false
