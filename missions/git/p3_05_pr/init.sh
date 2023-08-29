f1b5062f82e1d40ee558a5782e6df04cb70d0ec7
balbal <olivier.mattelaer@uclouvain.be>
2023-08-29 10:35:40 +0200

    adding level for pull request

---
+++ missions/git/p3_05_pr/init.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+
+if [ ! -d "$GSH_HOME/Factory" ]
+then
+   mkdir "$GSH_HOME/Factory"
+fi
+   
+### SET/RESET file has expected at the end of previous mission 
+if [ ! -d "$GSH_HOME/Factory/gitlectures" ]
+then
+    cd $GSH_HOME/Factory
+    if [ ! -e "$GSH_HOME/.fork" ]
+    then 
+       read -p "Please specify the github address of your fork:" fork
+       git clone $fork
+       echo $yn > $GSH_HOME/.fork
+    else
+	fork=$(cat $GSH_HOME/.fork)
+	git clone $fork
+    fi
+    cd $GSH_HOME/Factory/gitlectures
+    # check that the file is set at initial valuee
+fi
+fork=$(cat $GSH_HOME/.fork)
+
+
+cd $GSH_HOME/Factory/gitlectures
+
+
+
+
+
+
+
+
