36f9afe795a0bb2ad66fc2260f58d05c98b7a16a
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2023-12-11 09:39:14 +0100

    add security against wrong clone (with ssh this time)

--- missions/git/p3_01_github/check.sh
+++ missions/git/p3_01_github/check.sh
@@ -8,11 +8,16 @@ then
     fork=$(git remote get-url origin)
     if [[ $fork == "https://github.com/oliviermattelaer/gitlectures" ]]
     then
-	false
+        echo "You did clone the original repo, you do not have write access on that one. Please create a fork"
+	    false
+    elif [[ $fork == "git@github.com:oliviermattelaer/gitlectures.git" ]]
+    then
+        echo "You did clone the original repo, you do not have write access on that one. Please create a fork"
+	    false
     else
-	echo $fork > $GSH_HOME/.fork
-	echo "Good Work"
-	true
+	    echo $fork > $GSH_HOME/.fork
+	    echo "Good Work"
+	    true
     fi
 else
     echo "you did not clone the repo"
