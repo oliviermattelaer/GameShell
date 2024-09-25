5c60f64b0c0594f9bea1d3345a83a267cc02f27c
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2024-09-25 16:56:20 +0200

    avoid module disable

--- missions/lmod/07_eb/init.sh
+++ missions/lmod/07_eb/init.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 cd "$GSH_HOME"
-module disable VESUVE 2&> /dev/null
+#module disable VESUVE 2&> /dev/null
 #module purge
 
-cd "$GSH_HOME"
+#cd "$GSH_HOME"
 
 if [ ! -e .local ]; then
         mkdir .local
