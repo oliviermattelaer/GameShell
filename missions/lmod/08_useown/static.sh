f7c1229de32762b477da8cff2cead6e3478de694
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2024-09-26 11:53:34 +0200

    use static.sh -> fix syntax

--- missions/lmod/08_useown/static.sh
+++ missions/lmod/08_useown/static.sh
@@ -1,3 +1,3 @@
 mkdir -p "$GSH_HOME/.local/modules"
 mkdir -p "$GSH_HOME/Movies/gl_1971"
-mkdir -p "$GSH_HOME/Movies/lib/site-package/python3.10/
+mkdir -p "$GSH_HOME/Movies/lib/site-package/python3.10/"
