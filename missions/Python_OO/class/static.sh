fd1539d7b25add304a0e1d86c6144941dd992afa
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2024-11-12 21:25:27 +0100

    change handling for vimrc

--- missions/Python_OO/class/static.sh
+++ missions/Python_OO/class/static.sh
@@ -2,7 +2,7 @@ if [ -e ${REAL_HOME}/.vimrc ]
 then
     cp "${REAL_HOME}/.vimrc" "${GSH_HOME}/.vimrc"
 else 
-    cp "$MISSION_DIR/.vimrc" "${GSH_HOME}/.vimrc"
+    cp "$MISSION_DIR/vimrc" "${GSH_HOME}/vimrc"
 fi
 
 
