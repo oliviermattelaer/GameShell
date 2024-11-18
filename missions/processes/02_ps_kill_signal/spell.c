f91fae1a19901dc4cf59965fca99f12875378efc
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2024-11-18 13:39:48 +0100

    fix semaphore

--- missions/processes/02_ps_kill_signal/spell.c
+++ missions/processes/02_ps_kill_signal/spell.c
@@ -119,11 +119,18 @@ int main()
     }
 
     // Initialize semaphores.
-    printing_sem = sem_open("/printing_sem", O_CREAT, 0644, 1);
+    //
+     char login[20];
+     get_login(login, 20);
+     char sem_path[30];
+     strcopy(sem_path, "/writing_sem_");
+     strcat(sem_path,login);
+
+    printing_sem = sem_open(sem_path, O_CREAT, 0644, 1);
     if (printing_sem == SEM_FAILED)
         return 1;
 
-    writing_sem = sem_open("/writing_sem", O_CREAT, 0644, 1);
+    writing_sem = sem_open(sem_path, O_CREAT, 0644, 1);
     if (writing_sem == SEM_FAILED)
         return 1;
 
