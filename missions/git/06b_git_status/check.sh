85df27a1230a58a51c1d692200a698ac9490ae43
oliviermattelaer <33414646+oliviermattelaer@users.noreply.github.com>
2025-10-10 10:28:34 +0200

    Enhance echo messages in check.sh for clarity
    Improve feedback messages for file restoration checks.

--- missions/git/06b_git_status/check.sh
+++ missions/git/06b_git_status/check.sh
@@ -33,7 +33,8 @@ then
     echo "$(gettext "Your file should_be_restored_to_last_commit_WARNING_change_already_pushed_in_index.txt is still not in a clean state. Did you restore the file?")"
 elif git status should_be_restored_to_last_commit_WARNING_change_already_pushed_in_index.txt | grep "Changes not staged for commit" >& /dev/null
 then
-     echo "$(gettext "Your file should_be_restored_to_last_commit_WARNING_change_already_pushed_in_index.txt is still not in a clean state. Your modified file is not in the staging anymore but you ALSO need to restored the one in your working directory.")"
+     echo "$(gettext "You are on the right track ...
+     echo "$(gettext "But your file should_be_restored_to_last_commit_WARNING_change_already_pushed_in_index.txt is still not in a clean state. Your modified file is not in the staging anymore (good) but you ALSO need to restored the one in your working directory.")"
 elif cat should_be_restored_to_last_commit_WARNING_change_already_pushed_in_index.txt | grep "number of customer is 0" >& /dev/null;
 then
 	
